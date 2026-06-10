52db614c87813ea5e4cdf88d0eed565cde0dd91f
Drivasx <davidmrivasb2004@gmail.com>
2026-06-09 19:16:37 -0500

    fix: resolve nativewind issue

---
+++ mobile/app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center bg-red-100"
+      
+    >
+      <Text>Hola mundo</Text>
+    </View>
+  );
+}
