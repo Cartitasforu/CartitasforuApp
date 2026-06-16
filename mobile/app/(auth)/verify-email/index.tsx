6c715881af8b319877a64339252f90aeedfa4b8b
Drivasx <davidmrivasb2004@gmail.com>
2026-06-16 13:02:43 -0500

    feat: add otp form functionality

--- mobile/app/(auth)/verify-email/index.tsx
+++ mobile/app/(auth)/verify-email/index.tsx
@@ -2,9 +2,39 @@ import { AppButton } from "@/components/ui/app-button";
 import { AppInput } from "@/components/ui/app-input";
 import { AppText } from "@/components/ui/app-text";
 import { router } from "expo-router";
+import { ChangeEvent, useState } from "react";
 import { View, Text, TouchableOpacity, Pressable, Alert } from "react-native";
 
+interface VerificationCode {
+  input1: string,
+  input2: string,
+  input3: string,
+  input4: string,
+  input5: string,
+  input6: string,
+}
+
 export default function VerifyEmailScreen() {
+  const [code, setCode] = useState<VerificationCode>({
+    input1: '',
+    input2: '',
+    input3: '',
+    input4: '',
+    input5: '',
+    input6: ''
+  })
+
+  
+
+  const codeDigits: string = `${code.input1}${code.input2}${code.input3}${code.input4}${code.input5}${code.input6}`; 
+
+  const handleVerify = () => {
+    console.log(codeDigits)
+    Alert.alert(codeDigits)
+  }
+
+
+
   return (
     <View className="flex-1 bg-bgPink px-10 pt-10">
       <TouchableOpacity onPress={() => router.replace("/signup")}>
@@ -28,11 +58,43 @@ export default function VerifyEmailScreen() {
           </AppText>
         </View>
       </View>
-      <View className="flex flex-row gap-6 items-center justify-center">
-        <AppInput className="w-16 h-16 text-center" />
-        <AppInput className="w-16 h-16 text-center" />
-        <AppInput className="w-16 h-16 text-center" />
-        <AppInput className="w-16 h-16 text-center" />
+      <View className="flex flex-row gap-3 items-center justify-center">
+        <AppInput
+          className="w-12 h-12 text-center"
+          maxLength={1}
+          value={code.input1}
+          onChangeText={(newInput) => setCode({ ...code, input1: newInput })}
+        />
+        <AppInput
+          className="w-12 h-12 text-center"
+          maxLength={1}
+          value={code.input2}
+          onChangeText={(newInput) => setCode({ ...code, input2: newInput })}
+        />
+        <AppInput
+          className="w-12 h-12 text-center"
+          maxLength={1}
+          value={code.input3}
+          onChangeText={(newInput) => setCode({ ...code, input3: newInput })}
+        />
+        <AppInput
+          className="w-12 h-12 text-center"
+          maxLength={1}
+          value={code.input4}
+          onChangeText={(newInput) => setCode({ ...code, input4: newInput })}
+        />
+        <AppInput
+          className="w-12 h-12 text-center"
+          maxLength={1}
+          value={code.input5}
+          onChangeText={(newInput) => setCode({ ...code, input5: newInput })}
+        />
+        <AppInput
+          className="w-12 h-12 text-center"
+          maxLength={1}
+          value={code.input6}
+          onChangeText={(newInput) => setCode({ ...code, input6: newInput })}
+        />
       </View>
       <View className="flex flex-row items-center justify-center pt-4 pb-6">
         <AppText>No recibiste el código? </AppText>
@@ -40,11 +102,7 @@ export default function VerifyEmailScreen() {
           <Text className="text-primaryDeep">Reenviar</Text>
         </Pressable>
       </View>
-      <AppButton
-        title="Verificar"
-        onPress={() => Alert.alert("verificado")}
-        className="mt-2"
-      />
+      <AppButton title="Verificar" onPress={handleVerify} className="mt-2" />
     </View>
   );
 }
