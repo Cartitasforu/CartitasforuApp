48d64c2ee624d12a80a2d09f682bacf915e1e8a3
Yeiner99SD <yeinermoreno2005@gmail.com>
2026-06-19 21:17:25 -0500

    ft: commit inicial para la rama de rest-password

--- mobile/app/(onboarding)/index.tsx
+++ mobile/app/(onboarding)/index.tsx
@@ -1,10 +1,23 @@
+import { logOut } from "@/features/auth/api/log-out";
 import React from "react";
-import { Text, View } from "react-native";
+import { Pressable, Text, TouchableOpacity, View } from "react-native";
+import { router } from "expo-router";
+
 
 export default function OnboardingScreen() {
+  const onLogOut = async () => {
+    await logOut();
+  };
+
   return (
     <View>
       <Text>OnboardingScreen</Text>
+      <TouchableOpacity onPress={() => router.replace("/(splash)")}>
+        <Text>Volver</Text>
+      </TouchableOpacity>
+      <Pressable onPress={onLogOut}>
+        <Text>Cerrar sesión</Text>
+      </Pressable>
     </View>
   );
 }
