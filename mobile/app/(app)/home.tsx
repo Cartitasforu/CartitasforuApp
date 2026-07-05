8123f6e2ecbb8efb94c837dfdaaa55f4c7a91b8a
Yeiner99SD <yeinermoreno2005@gmail.com>
2026-07-05 18:43:03 -0500

    add: agregué modo de desarrollo

--- mobile/app/(app)/home.tsx
+++ mobile/app/(app)/home.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 
 export default function HomeScreen () {
   return (
-    <View>home</View>
+    <View>
+      <Text>home</Text>
+    </View>
   )
 }
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,11 +1,12 @@
-import AuthProvider from "@/providers/AuthProvider";
-import "../global.css";
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+  duration: 1000,
+  fade: true,
+});
 
 export default function RootLayout() {
-  return (
-  <AuthProvider>
-    <Stack screenOptions={{headerShown: false}}/>
-  </AuthProvider>
-  )
+  return <Stack />;
 }
