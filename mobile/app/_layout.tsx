83e28554f219c328e4c6fbec41cf181666fbb3a1
Yeiner99SD <yeinermoreno2005@gmail.com>
2026-07-11 23:30:06 -0500

    commit prueba

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,12 +1,17 @@
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
+import AuthProvider from '@/providers/AuthProvider';
 
 // Set the animation options. This is optional.
 SplashScreen.setOptions({
-  duration: 10000,
+  duration: 1000,
   fade: true,
 });
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <AuthProvider>
+      <Stack />
+    </AuthProvider>
+  );
 }
